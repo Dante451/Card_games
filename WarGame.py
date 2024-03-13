b87727b6850613ffd456637c1e7956df62a350a6
Dante451 <dantedaciuk555@gmail.com>
2024-03-13 17:29:01 -0500

    Created a card in terminal and added four ascii card suit symbols

---
+++ WarGame.py
@@ -0,0 +1,19 @@
+"""
+War card game
+Dante Daciuk
+13/3/24
+"""
+
+print(" __________")
+print("|          |")
+print("|          |")
+print("|          |")
+print("|          |")
+print("|          |")
+print("|__________|")
+print("")
+print("⯁ diamond")
+print("♠ spade")
+print("♣️ club")
+print("♥ heart")
+
